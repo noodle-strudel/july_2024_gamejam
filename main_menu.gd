ffe2ba2c44e5fabb87fbbdffcac9fbf4ca465a3d
noodle-strudel <yollaine369@gmail.com>
2024-07-11 19:03:19 -0700

    Prototyped Settings Menu Added volume slider Added 4 windowed resolution options Added display options (windowed, fullscreen)

---
+++ main_menu.gd
@@ -0,0 +1,54 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_volume_value_changed(value):
+	if value == 0:
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
+		AudioServer.set_bus_volume_db(0, value)
+
+
+func _on_resolution_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(2560, 1440))
+		1:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		2:
+			DisplayServer.window_set_size(Vector2i(1366, 768))
+		3:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+
+
+func _on_option_button_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_size(DisplayServer.window_get_size())
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
+func _on_settings_button_pressed():
+	$Settings.show()
+
+
+func _on_settings_back_button_pressed():
+	$Settings.hide()
