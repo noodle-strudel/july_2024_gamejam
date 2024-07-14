c8f9d9cd77d7f0ee1d8ddb4984e5850fe0961cbc
palnex <thepalnex010@gmail.com>
2024-07-14 00:24:29 -0700

    Volume slider fix, and start button sound fix.

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Music.play()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,11 +17,9 @@ func _on_quit_button_pressed():
 
 
 func _on_volume_value_changed(value):
-	if value == 0:
-		AudioServer.set_bus_mute(0, true)
-	else:
-		AudioServer.set_bus_mute(0, false)
-		AudioServer.set_bus_volume_db(0, value)
+	
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 
+		linear_to_db($Settings/MarginContainer/VBoxContainer/Volume.value))
 
 
 func _on_resolution_item_selected(index):
@@ -66,4 +64,5 @@ func _on_settings_back_button_pressed():
 
 func _on_start_button_pressed():
 	$ClickSFX.play()
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://game.tscn")
