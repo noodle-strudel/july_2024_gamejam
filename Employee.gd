585eb3d5a6819e2c9d3530741e81e2942f998bda
MrSqueeg <thagoglia@gmail.com>
2024-07-14 13:41:20 -0700

    Base Task claiming system and Task Timers

--- Employee.gd
+++ Employee.gd
@@ -1,31 +1,30 @@
 extends Area2D
+signal newTask(value)
+signal taskComplete(value)
 
+# Setup Var
+var task
+var goal
 var taskActive = false
 var taskRequested = true
 var taskCompleted = true
 var rng = RandomNumberGenerator.new()
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+# Check if player collides with Employee
 func _on_body_entered(body):
+	# See if task is requested from player and start
 	if (taskActive == false && taskRequested == true):
 		taskActive = true
-		var task = rng.randi_range(0, 10)
+		task = rng.randi_range(0, 10)
+		emit_signal("newTask", task)
 		print("Starting Task Number: ", task)
 		return
-
+	
+	# Reset for task completion
 	if (taskCompleted):
 		taskActive = false
 		taskRequested = true
+		emit_signal("taskComplete", task)
 		print("Task Completed")
 		return
 		
--- playerScript.gd
+++ playerScript.gd
@@ -1,22 +1,77 @@
 extends CharacterBody2D
 
+# Var Init
 var speed = 600.0
 var accel = 5
 var rng = RandomNumberGenerator.new()
-var tasks: Array = []
+var tasks : Array[Task] = []
 var taskCount = 0
-
 var input: Vector2
 
+# Task Class Setup
+class Task:
+	var desc : String
+	var timeRemaining : Timer
+	var complete : bool
+	var taskID : int
+	var taskGoal : int
+	
+# Testing
+func _ready():
+	get_tree().call_group("Employees", "on_employee_new_task")	
+
+# Movement Input
 func get_input():
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	return input.normalized()
-	
+
+# Apply Movement
 func _process(delta):
 	var playerInput = get_input()
-	
 	velocity = lerp(velocity, playerInput * speed, delta * accel)
-	
 	move_and_slide()
+			
 
+# Setup Task upon Claim
+func _on_employee_new_task(value):
+	# Timer Setup
+	var timer : Timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+	timer.autostart = true
+	timer.wait_time = 5.0
+	timer.timeout.connect(_timer_Timeout)
+	timer.start()
+	
+	# Task Setup
+	var newTask = Task.new()
+	newTask.desc = "desc"
+	newTask.timeRemaining = timer
+	newTask.taskID = value
+	tasks.append(newTask)
+	++taskCount
+	print(tasks)
+
+# Find task to remove on completion
+func _on_employee_task_complete(value):
+	for i in tasks:
+		print(i.taskID)
+		if (i.taskID == value):
+			var index = tasks.find(i)
+			tasks.remove_at(index)
+			print("Removed ", i, " at index ", index)
+
+func _timer_Timeout():
+	for i in tasks:
+		if (i.timeRemaining.time_left <= 0):
+			var index = tasks.find(i)
+			tasks.remove_at(index)
+			print("Timeout time left: ", i.timeRemaining.time_left, " ID ", i.taskID)
+# Repeat code for additonal employees need to find way to combine signals
+func _on_employee_2_new_task(value):
+	_on_employee_new_task(value)
+
+
+func _on_employee_2_task_complete(value):
+	_on_employee_task_complete(value)
